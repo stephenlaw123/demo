d14b246593943719598fedfbfe1805ddc6c1aeeb
law <owen_son@yahoo.com>
2023-05-30 21:36:00 +0800

    second commit

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -1,3 +1,5 @@
+package com.example.demo;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
